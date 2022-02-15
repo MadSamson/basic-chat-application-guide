e9239f0871d93993ccbf4ee816c5f40f7fc8ce0c
Kriksy <viktoria.toldi@gmail.com>
2022-02-15 14:22:46 +0100

    Add serving HTML

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world!</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
-
-
-// This means that:
-// Express initializes app to be a function handler 
-// that you can supply to an HTTP server(as seen in line 4).
-// We define a route handler / that gets called when we hit our website home.
-// We make the http server listen on port 3000.
-
-// We used: npm install express@4
