58e4abf2f19027fc4ea0879688a543f5abd864a1
Kriksy <viktoria.toldi@gmail.com>
2022-02-15 14:16:09 +0100

    Add web framework

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+
+// This means that:
+// Express initializes app to be a function handler 
+// that you can supply to an HTTP server(as seen in line 4).
+// We define a route handler / that gets called when we hit our website home.
+// We make the http server listen on port 3000.
+
+// We used: npm install express@4
